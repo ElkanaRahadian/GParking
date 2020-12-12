85cc90692980ee4df37d8e31460344ca594b44bf
Elkana Antonio <elkanarahadian@gmail.com>
2020-12-12 23:58:22 +0700

    Update Registration.js

--- js/Registration.js
+++ js/Registration.js
@@ -1,4 +1,4 @@
-var fullnam = document.getElementById("txtname");
+var fullname = document.getElementById("txtname");
 var email = document.getElementById("txtemail");
 var username = document.getElementById("txtusername");
 var password = document.getElementById("txtpassword");
